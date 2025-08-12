91983d512fb8e58c1dc6d907f4c1dd806e8c3429
ALEX HENRIQUE DA CRUZ GOMES <25132769@CFP127ALUNO.LOCAL>
2025-08-12 13:40:36 -0300

    comeco da adição das legendas

--- main.js
+++ main.js
@@ -3,7 +3,10 @@
 function criarImagem(imagem){
         const galeria = document.getElementById('galeria')
         const img = document.createElement('img')
+        const legenda = document.createElement('p')
         img.src = imagem.url
+        legenda.textContent = 
+
 
         galeria.appendChild(img)
 }
@@ -12,43 +15,43 @@ function carregarImagens(){
     const imagens = [
         {
             nome: 'power_rangers1',
-            url: './img/6r4d92bhnu491.png'
+            url: './img/power1.png'
         },
         {
             nome: 'power_rangers2',
-            url: './img/24-years-later-i-m-convinced-power-rangers-fans-give-this-season-too-much-credit.avif'
+            url: './img/power2.png'
         },
         {
             nome: 'power_rangers3',
-            url: './img/All_10_Kyoryugers.webp'
+            url: './img/power9.png'
         },
         {
             nome: 'power_rangers4',
-            url: './img/fdf5ef546f035509e27e83ee9484a9412c0e8d57a9da257e6361f74236e75068._SX1080_FMjpg_.jpg'
+            url: './img/power3.png'
         },
         {
             nome: 'power_rangers5',
-            url: './img/image-asset.webp'
+            url: './img/power4.png'
         },
         {
             nome: 'power_rangers6',
-            url: './img/Power-Rangers-ZEO-Toku-Blog.webp'
+            url: './img/power5.png'
         },
         {
             nome: 'power_rangers7',
-            url: './img/r333jubri0341.jpg'
+            url: './img/power6.png'
         },
         {
             nome: 'power_rangers8',
-            url: './img/y0pv3jgak6od1.jpeg'
+            url: './img/power7.png'
         },
         {
             nome: 'power_rangers9',
-            url: './img/Turbo-Capa.png'
+            url: './img/power8.png'
         },
         {
             nome: 'power_rangers10',
-            url: './img/what-would-you-have-done-to-make-power-rangers-samurai-v0-xrkljkgirkfb1.webp'
+            url: './img/power10.png'
         },
         
     ]
